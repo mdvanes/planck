5dc9318873711e49966f580e302d0093d03a49c7
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-09-10 15:25:51 +0200

    add album view button

--- app/src/main/java/nl/mdworld/planck4/networking/SubsonicAlbumEntity.kt
+++ app/src/main/java/nl/mdworld/planck4/networking/SubsonicAlbumEntity.kt
@@ -1,51 +1,63 @@
 package nl.mdworld.planck4.networking
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class SubsonicArtistsResponse(
-    val sr: SubsonicArtistsResponse2
+    @SerialName("subsonic-response") val sr: SubsonicArtistsResponse2
 )
 
+@Serializable
 data class SubsonicArtistsResponse2(
-    val artists: SubsonicArtistsResponseContainer
+    @SerialName("artists") val artists: SubsonicArtistsResponseContainer
 )
 
+@Serializable
 data class SubsonicArtistsResponseContainer(
-    val index: List<SubsonicArtistIndex>
+    @SerialName("index") val index: List<SubsonicArtistIndex>
 )
 
+@Serializable
 data class SubsonicArtistIndex(
-    val name: String,
-    val artist: List<SubsonicArtistsEntity>
+    @SerialName("name") val name: String,
+    @SerialName("artist") val artist: List<SubsonicArtistsEntity>
 )
 
+@Serializable
 data class SubsonicArtistsEntity(
-    val id: String,
-    val name: String,
-    val albumCount: Int = 0,
-    val coverArt: String? = null
+    @SerialName("id") val id: String,
+    @SerialName("name") val name: String,
+    @SerialName("albumCount") val albumCount: Int = 0,
+    @SerialName("coverArt") val coverArt: String? = null
 )
 
+@Serializable
 data class SubsonicAlbumsResponse(
-    val sr: SubsonicAlbumsResponse2
+    @SerialName("subsonic-response") val sr: SubsonicAlbumsResponse2
 )
 
+@Serializable
 data class SubsonicAlbumsResponse2(
-    val artist: SubsonicArtistDetail
+    @SerialName("artist") val artist: SubsonicArtistDetail
 )
 
+@Serializable
 data class SubsonicArtistDetail(
-    val id: String,
-    val name: String,
-    val albumCount: Int = 0,
-    val album: List<SubsonicAlbumsEntity>
+    @SerialName("id") val id: String,
+    @SerialName("name") val name: String,
+    @SerialName("albumCount") val albumCount: Int = 0,
+    @SerialName("album") val album: List<SubsonicAlbumsEntity>
 )
 
+@Serializable
 data class SubsonicAlbumsEntity(
-    val id: String,
-    val name: String,
-    val artist: String,
-    val artistId: String,
-    val songCount: Int = 0,
-    val duration: Int = 0,
-    val coverArt: String? = null,
-    val year: Int? = null
+    @SerialName("id") val id: String,
+    @SerialName("name") val name: String,
+    @SerialName("artist") val artist: String,
+    @SerialName("artistId") val artistId: String,
+    @SerialName("songCount") val songCount: Int = 0,
+    @SerialName("duration") val duration: Int = 0,
+    @SerialName("coverArt") val coverArt: String? = null,
+    @SerialName("year") val year: Int? = null
 )
