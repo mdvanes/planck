7db682313e01b062713d6f6e73ac5bca3533ea7d
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-09-17 14:42:32 +0200

    update target minSdk 24 to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "nl.mdworld.planck4"
-        minSdk = 24
+        minSdk = 30
         targetSdk = 36
         versionCode = 18
         versionName = "1.0.18"
