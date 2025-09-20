2865d561ed5e385fd93571463e924eaf943055f8
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-09-20 20:14:43 +0200

    disable statusBarColor and change palette

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "nl.mdworld.planck4"
         minSdk = 30
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.0.18"
+        versionCode = 19
+        versionName = "1.0.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
