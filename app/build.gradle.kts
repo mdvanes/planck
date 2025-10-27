6dd91e5f1dfa6a98bd65d14ceb60b3c683cac854
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-10-27 21:09:20 +0100

    add android:appCategory="audio"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "nl.mdworld.planck4"
         minSdk = 30
         targetSdk = 36
-        versionCode = 34
-        versionName = "1.0.34"
+        versionCode = 35
+        versionName = "1.0.35"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
